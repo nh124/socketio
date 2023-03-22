89034e3743c8dfc83c4aeb9f9d0044be06b9602d
nh124 <nur.haque99@gmail.com>
2023-03-21 22:58:59 -0400

    test

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app =  Flask(__name__)
+app.config['SECRET_KEY'] ='secret!'
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+@socketio.on('message')
+def send_message(message):
+    print(message)
+    if message != 'User connected':
+        send(message, broadcast=True)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, host="localhost", port=5000)
+    # app.run(debug=True)
